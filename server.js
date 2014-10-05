7b92fc7eb08d4134cf965737e05ea2cd857a1b5d
Matt Warren <mattwarrenmail@gmail.com>
2014-10-04 17:39:08 -0700

    updated style, now logs list of users when user logs on, now logs when a user leaves. #megacommit

--- server.js
+++ server.js
@@ -1,25 +1,40 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io= require('socket.io')(http);
+var io = require('socket.io')(http);
 var connect = require('connect');
 var serveStatic = require('serve-static');
+var port = process.env.PORT || 3000;
+app.use(serveStatic(__dirname + '/public'));
 
-app.use(serveStatic(__dirname + '/public')); 
-
-app.get('/', function(req, res){
-	res.sendFile(__dirname + '/index.html');
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-	console.log('a user connected');
-	socket.on('disconnect', function(){
-		console.log('user disconnected');
-	});
-	socket.on('chat message', function(msg){
-		io.emit('chat message', msg);
-	});
+var users = [
+];
+io.on('connection', function(socket) {
+    socket.on('disconnect', function() {
+        for (var i =0; i < users.length; i++){
+            if (users[i].id === socket.id){
+                io.emit('user left', users[i].name);
+                users.splice(i, 1);
+                break;
+            }
+        }
+    });
+    socket.on('chat message', function(msg) {
+        io.emit('chat message', msg);
+    });
+    
+    socket.on('add user', function(username) {
+        console.log(username + 'added');
+        users.push({id: socket.id, name: username});
+        io.emit('new user', username);
+        io.emit('user list', users.map(function(user){return user.name;}));
+    });
 });
 
-http.listen(3000, function(){
-	console.log('listening on *:3000!');
+http.listen(port, function() {
+    console.log('listening on *:3000!');
 });
+ 
